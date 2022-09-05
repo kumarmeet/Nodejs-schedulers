8315159d15b41ecd68a5dc612b88f88202a7a30c
Meet Kumar Vishwakarma <36449204+kumarmeet@users.noreply.github.com>
2022-09-05 15:32:41 +0530

    Update cron.js

--- cron.js
+++ cron.js
@@ -1,3 +1,16 @@
+const dates = data.map((d) => {
+			let storedDates = moment(d.date).format("YYYY-MM-DD");
+			let hasPreviousDate = moment(storedDates).isAfter(presentDate, "day");
+
+			if (!hasPreviousDate) {
+				throw new Error("Invalid Expiry Date");
+			}
+
+			return new Date(
+				moment(storedDates).subtract(7, "d").format("YYYY-MM-DD")
+			);
+		});
+
 const monthDay = dates.map((date) => {
 			return {
 				month: moment(date).format("MMMM"),
